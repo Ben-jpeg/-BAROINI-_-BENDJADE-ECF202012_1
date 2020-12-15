ec0d609e8752da0127226aef3ffc46b5c4496453
Ben-jpeg <b.baroini@gmail.com>
2020-12-15 09:33:22 +0100

    page artist maj 15decmatin

--- scripts/scrollreveal.js
+++ scripts/scrollreveal.js
@@ -1,5 +1,5 @@
 ScrollReveal().reveal('h2', { delay: 200 });
-ScrollReveal().reveal('img', { delay: 800 });
-ScrollReveal().reveal('article', { delay: 800 });
-ScrollReveal().reveal('p', { delay: 800 });
-ScrollReveal().reveal('i', { delay: 800 });
+ScrollReveal().reveal('img', { delay: 600 });
+ScrollReveal().reveal('article', { delay: 600 });
+ScrollReveal().reveal('p', { delay: 600 });
+ScrollReveal().reveal('i', { delay: 600 });
